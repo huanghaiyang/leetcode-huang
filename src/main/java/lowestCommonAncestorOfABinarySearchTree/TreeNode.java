5fab02672dd2ca7829472b92b93e7011bfbd5614
huanghaiyang <haiyang.huang@forceclouds.com>
2019-06-12 17:06:10 +0800

    update

---
+++ src/main/java/lowestCommonAncestorOfABinarySearchTree/TreeNode.java
@@ -0,0 +1,13 @@
+package lowestCommonAncestorOfABinarySearchTree;
+
+/**
+ * created by huang 5/14/19
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+
+}
